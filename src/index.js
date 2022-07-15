eae7bbd4b9b9e31c357190affb1cf0f3c40a3c9e
SheshkoPavel <derzurb@mail.ru>
2022-07-15 14:44:33 +0300

    feat: solve  task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
+// const config1 = [['(', ')']];
+// const config2 = [['(', ')'], ['[', ']']];
+// const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+// const config4 = [['|', '|']];
+// const config5 = [['(', ')'], ['|', '|']];
+// const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+// const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+// const str = '([{}])';
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+//  function check(str, bracketsConfig) {
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  console.log(brackets);
+
+  let arr = [];
+
+  for (let bracket of str){
+    let index = brackets.indexOf(brackets);
+
+    if (index % 2 === 0) {
+
+      if (bracket === brackets[index + 1] && arr[arr.length - 1] === index) {
+        arr.pop();
+      } else if (bracket === brackets[index + 1] && arr[arr.length -1] !== index) {
+        arr.push(index);
+      } else {
+        arr.push(index);
+      }
+    }
+    else {
+      if (arr.pop() !== index-1) {
+        return false;
+      }
+    }
+
+  }
+  return arr.length === 0
+
 }
